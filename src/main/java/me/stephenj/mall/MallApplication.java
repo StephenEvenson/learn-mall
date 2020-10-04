4654f05343d5d89be67cedb296e4654add80d3c7
Stephen Evenson <1258455347@qq.com>
2020-10-04 15:37:47 +0800

    服务器时区问题

--- src/main/java/me/stephenj/mall/MallApplication.java
+++ src/main/java/me/stephenj/mall/MallApplication.java
@@ -3,8 +3,16 @@ package me.stephenj.mall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MallApplication {
+
+//    @PostConstruct
+//    void started() {
+//        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8:00"));
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(MallApplication.class, args);
