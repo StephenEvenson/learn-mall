7ad11974dfbfc2dae6c1b17877da376473328d20
Stephen Evenson <1258455347@qq.com>
2020-10-05 17:50:18 +0800

    在config中添加Spring Task配置

---
+++ src/main/java/me/stephenj/mall/config/SpringTaskConfig.java
@@ -0,0 +1,14 @@
+package me.stephenj.mall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 定时任务配置
+ * Created by stephen on 2020/10/5.
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
+
