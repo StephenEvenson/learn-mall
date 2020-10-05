639597ce8eef6dbb3613e4748e33458abc31cd77
Stephen Evenson <1258455347@qq.com>
2020-10-05 08:57:18 +0800

    在generatorConfig.xml添加配置，来生成后台用户表、后台用户角色表、后台用户权限表、后台用户与角色关系表、后台用户角色与权限关系表、后台用户与权限关系表(只包含加减权限)

---
+++ src/main/java/me/stephenj/mall/mbg/mapper/UmsRolePermissionRelationMapper.java
@@ -0,0 +1,30 @@
+package me.stephenj.mall.mbg.mapper;
+
+import java.util.List;
+import me.stephenj.mall.mbg.model.UmsRolePermissionRelation;
+import me.stephenj.mall.mbg.model.UmsRolePermissionRelationExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface UmsRolePermissionRelationMapper {
+    long countByExample(UmsRolePermissionRelationExample example);
+
+    int deleteByExample(UmsRolePermissionRelationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsRolePermissionRelation record);
+
+    int insertSelective(UmsRolePermissionRelation record);
+
+    List<UmsRolePermissionRelation> selectByExample(UmsRolePermissionRelationExample example);
+
+    UmsRolePermissionRelation selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsRolePermissionRelation record, @Param("example") UmsRolePermissionRelationExample example);
+
+    int updateByExample(@Param("record") UmsRolePermissionRelation record, @Param("example") UmsRolePermissionRelationExample example);
+
+    int updateByPrimaryKeySelective(UmsRolePermissionRelation record);
+
+    int updateByPrimaryKey(UmsRolePermissionRelation record);
+}
