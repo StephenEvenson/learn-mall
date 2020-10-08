d1de4663b5296a41156b23edbbfc163fe9e8122c
Stephen Evenson <1258455347@qq.com>
2020-10-08 16:36:53 +0800

    在 nosql/mongodb/repository 中，添加Repository接口，用于操作mongodb

---
+++ src/main/java/me/stephenj/mall/nosql/mongodb/repository/MemberReadHistoryRepository.java
@@ -0,0 +1,19 @@
+package me.stephenj.mall.nosql.mongodb.repository;
+
+
+import me.stephenj.mall.nosql.mongodb.document.MemberReadHistory;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+/**
+ * 会员商品浏览历史Repository
+ * Created by stephen on 2020/10/6.
+ */
+public interface MemberReadHistoryRepository extends MongoRepository<MemberReadHistory,String> {
+    /**
+     * 根据会员id按时间倒序获取浏览记录
+     * @param memberId 会员id
+     */
+    List<MemberReadHistory> findByMemberIdOrderByCreateTimeDesc(Long memberId);
+}
