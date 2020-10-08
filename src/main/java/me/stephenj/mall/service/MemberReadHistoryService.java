5b07d7f6e0255e4f7377d3a1a8df6ac218b4f25b
Stephen Evenson <1258455347@qq.com>
2020-10-08 16:37:10 +0800

    在 service, service/impl 和 controller 中添加用户浏览记录的接口类、实现类和控制类

---
+++ src/main/java/me/stephenj/mall/service/MemberReadHistoryService.java
@@ -0,0 +1,27 @@
+package me.stephenj.mall.service;
+
+
+import me.stephenj.mall.nosql.mongodb.document.MemberReadHistory;
+
+import java.util.List;
+
+/**
+ * 会员浏览记录管理Service
+ * Created by stephen on 2020/10/6.
+ */
+public interface MemberReadHistoryService {
+    /**
+     * 生成浏览记录
+     */
+    int create(MemberReadHistory memberReadHistory);
+
+    /**
+     * 批量删除浏览记录
+     */
+    int delete(List<String> ids);
+
+    /**
+     * 获取用户浏览历史记录
+     */
+    List<MemberReadHistory> list(Long memberId);
+}
